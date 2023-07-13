8604069258f8cab1690da21c6d7dd0586bc370ac
Precious Aleaji <legenduz15@gmail.com>
2023-07-13 23:53:30 +0000

    Betty-0 :)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-/**
- * This function prints the text
- * 'Programming is like building a multilingual puzzle'
- */
+/* A puts function */
 
 int main(void)
+	/* This is main*/
 {
 	char message[] = "\"Programming is like building a multilingual puzzle";
 
 
 	puts(message);
+
+/* To always return 0 */
 
 	return (0);
 }
