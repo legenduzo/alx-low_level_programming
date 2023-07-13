c1b4f3df3e4c81b556f8cfc4892f1c2a32bd8294
Precious Aleaji <legenduz15@gmail.com>
2023-07-13 23:37:42 +0000

    Updated 4-0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include <string.h>
+/**
+ * This function prints the text
+ * 'Programming is like building a multilingual puzzle'
+ */
 
 int main(void)
 {
 	char message[] = "Programming is like building a multilingual puzzle";
+	char newLine[] = "\n";
+
+	strcat(message,newLine);
 
 	puts(message);
 
