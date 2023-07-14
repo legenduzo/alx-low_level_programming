e0d6d79b96fc7b9707e09132db2efc2f8d59e9e0
Precious Aleaji <legenduz15@gmail.com>
2023-07-14 01:02:23 +0000

    ADDED: 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* A printf function */
+
+/**
+ * main -  The entry point of the program
+ * Description: Prints something
+ *
+ * Return: Returns with an 0 (Always success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
