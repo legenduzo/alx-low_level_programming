088a24bbbc07399d1b11700ad89435556ff2c714
Precious Aleaji <legenduz15@gmail.com>
2023-07-14 02:38:37 +0000

    ADDED: 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <string.h>
+
+/* A function that prints to sderr */
+
+/**
+ * main - the entry point
+ * Description: A function that does not use printf and put
+ *
+ * Return: returns 1 (error)
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, strlen(message));
+
+	return (1);
+}
