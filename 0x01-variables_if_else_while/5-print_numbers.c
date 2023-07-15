f67f0007b8ca47158fcf2e300ade1b17910848ac
Precious Aleaji <legenduz15@gmail.com>
2023-07-15 02:13:53 +0000

    ADDED: 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* This function prints single digits and \n */
+
+/**
+ * main - the entry point
+ * Description: prints 0-9 and \n
+ *
+ * Return: 0 - success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		echo i;
+	}
+	putchar('\n');
+
+	return (0);
+}
