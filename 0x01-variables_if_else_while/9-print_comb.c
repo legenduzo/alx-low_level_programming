da1e22c3e7736c9e7ff7bea550ff92bff7adc1a0
Precious Aleaji <legenduz15@gmail.com>
2023-07-15 03:29:03 +0000

    ADDED: 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* Function prints 1-9 separated by commas */
+
+/**
+ * main - entry point
+ * Description: prints 0-9
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+		putchar(' ');
+		putchar(',');
+	}
+	putchar('\n');
+
+	return (0);
+}
