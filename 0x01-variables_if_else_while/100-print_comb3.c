ff994e3c9254775f9b43082664cc136e0e99a10c
Precious Aleaji <legenduz15@gmail.com>
2023-07-15 04:07:43 +0000

    ADDED: 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/* Function prints the smallest combo of 2 numbers */
+
+/**
+ * main - entry
+ * Description: prints a combo of numbers
+ *
+ * Return: 0 - success
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			if (i < n)
+			{
+				putchar((i * 10) + n + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n')
+
+}
