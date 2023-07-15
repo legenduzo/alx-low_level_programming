ce8b2326ec55da513ca68976c7c582b60273ec08
Precious Aleaji <legenduz15@gmail.com>
2023-07-15 04:42:42 +0000

    ADDED: 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/* Function prints the smallest combo of 3 numbers */
+
+/**
+ * main - entry
+ * Description: prints a combo of numbers
+ *
+ * Return: 0 - success
+ */
+
+int main(void)
+{
+	int i;
+	int n;
+	int k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (n = i + 1; n <= 8; n++)
+		{
+			for (k = i + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(n + '0');
+				putchar(k + '0');
+
+				if (i != 7 || n != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
