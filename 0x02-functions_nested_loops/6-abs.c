6e4a4ba785c34bc306c56790acc5ad1c36454bdc
Precious Aleaji <legenduz15@gmail.com>
2023-07-18 22:42:59 +0000

    ADDED: 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+/* This function returns absolute values */
+
+/**
+ * _abs - returns absolute numbers
+ * @n: number to return absolute of
+ *
+ * Return: integer
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
