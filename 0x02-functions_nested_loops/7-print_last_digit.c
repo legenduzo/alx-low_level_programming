3bde8d42366de0c8302cc64a4467e7ceada8f7cb
Precious Aleaji <legenduz15@gmail.com>
2023-07-18 22:52:33 +0000

    ADDED 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+/* Prints the last digit of a number */
+
+/**
+ * print_last_digit - does as its name says
+ * @n: number to extract last digit from
+ *
+ * Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
