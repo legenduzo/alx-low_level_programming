25247b92a5f2a5cb9a18a6dc29ef0010cd51cc7b
Precious Aleaji <legenduz15@gmail.com>
2023-07-18 14:50:43 +0000

    ADDED 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/* Prints a-z to std1 and \n */
+
+/**
+ * print_alphabet - prints a-z
+ *
+ * Return: return type is void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+}
