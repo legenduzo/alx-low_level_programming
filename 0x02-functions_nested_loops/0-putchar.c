6f5f84b719b253444f30df3b0b3c507fbac0da2f
Precious Aleaji <legenduz15@gmail.com>
2023-07-18 16:43:01 +0000

    Updated  0-putchar.c-1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /* This function prints _putchar to the standard output */
 
@@ -11,7 +12,28 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	const char *putchar_string = "_putchar";
+
+	for (int i = 0; putchar_string[i] != '\0'; i++)
+	{
+		_putchar(putchar_string[i]);
+	}
+	_putchar('\n');
 
 	return (0);
+}
+
+/* This function returns one character per time */
+
+/**
+ * _putchar - returns putchar
+ *
+ * Return: int
+ *
+ * Parameter: of type char
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
 }
