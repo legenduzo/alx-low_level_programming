aba6bf0d8b94fb02a70651a91536f5687e61bc5f
Precious Aleaji <legenduz15@gmail.com>
2023-07-18 14:15:15 +0000

    Updated 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-echo -e '_putchar\n'
+#include <stdio.h>
+
+/* This function prints _putchar to the standard output */
+
+/**
+ * main - entry point
+ * Description: prints putchar
+ *
+ * Return: return 0 - success
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return 0;
+}
