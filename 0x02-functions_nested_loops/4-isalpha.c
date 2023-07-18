77f8f6546e7a464883bfa2b44c564a7a3de1af47
Precious Aleaji <legenduz15@gmail.com>
2023-07-18 21:23:59 +0000

    ADDED: 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+/* This function checks the ascii value of a character */
+
+/**
+ * _isalpha - checks for alphabet equals
+ * @c: character to be accessed
+ *
+ * Return: 1 if alphabet, 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123 || c > 64 && c < 91)
+	{
+		return (1);
+	}
+
+	return (0);
+}
