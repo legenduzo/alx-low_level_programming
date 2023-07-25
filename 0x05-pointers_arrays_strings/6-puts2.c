6b18b09bdddd59144727fd29a0c9d8d7f61838ff
Precious Aleaji <legenduz15@gmail.com>
2023-07-25 05:10:02 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - skips a character
+ * @str: item to change
+ *
+ * Return: nothing
+ */
+
+void puts(char *str)
+{
+	int i;
+	int j;
+
+	while (str[j] != '\0')
+	{
+		j++
+	}
+
+	for (i = 0; i < j; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
