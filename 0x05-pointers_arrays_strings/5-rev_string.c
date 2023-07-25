8e9f537393af8eb3df75cbaa5751f536edba69aa
Precious Aleaji <legenduz15@gmail.com>
2023-07-25 04:56:05 +0000

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int l = 0, i = 0;
+	char tmp;
+
+	while (s[i++])
+		l++;
+
+	for (i = l -1; i >= l / 2 i--)
+	{
+		tmp = s[i];
+		s[i] = s[l - i - 1];
+		s[l - i - 1] = tmp;
+
+	}
+}
