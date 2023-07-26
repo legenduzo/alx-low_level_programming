320b5c1feffc91aa6c0690e7ad477df718b70cb5
Precious Aleaji <legenduz15@gmail.com>
2023-07-26 05:02:05 +0000

    100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _atoi - a
+ * @s: s
+ *
+ * Return: int
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int res = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				res = res * 10 + (s[i] - '0');
+				i++
+			}
+			return (res * sign);
+		}
+		i++
+	}
+	return (0);
+}
