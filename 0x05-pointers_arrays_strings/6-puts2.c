0ca2f0d53cb817f790be7285d1efd7aef66d5620
Precious Aleaji <legenduz15@gmail.com>
2023-07-26 04:30:46 +0000

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,17 +9,15 @@
 
 void puts(char *str)
 {
-	int i;
 	int j = 0;
 
 	while (str[j] != '\0')
 	{
+		if (j % 2 == 0)
+		{
+			_putchar(str[j]);
+		}
 		j++;
-	}
-
-	for (i = 0; i < j; i += 2)
-	{
-		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
