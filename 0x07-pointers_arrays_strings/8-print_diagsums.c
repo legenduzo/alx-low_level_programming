c1593bc0a8a3232f7a02553300e6ed898a36ee47
Precious Aleaji <legenduz15@gmail.com>
2023-08-01 03:37:33 +0000

    7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints diagsums
+ * @a: diagonal
+ * @size: size to work with
+ *
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, n;
+	int sum1 = 0, sum2 = 0;
+
+	for (i = 0; i <= (size * size); i = i + size + 1)
+	{
+		sum1 = sum1 + a[i];
+	}
+
+	for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
+	{
+		sum2 = sum2 + a[n];
+	}
+	printf("%d, %d\n", sum1, sum2);
+
+}
