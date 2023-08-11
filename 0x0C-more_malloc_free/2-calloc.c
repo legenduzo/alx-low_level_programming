618b31074a51bc367bc043f933996f9e092643e6
Precious Aleaji <legenduz15@gmail.com>
2023-08-11 02:20:41 +0000

    23

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -25,10 +25,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	p = s;
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
-		*(char *)p = 0;
-		p = (char *)p + size;
+		*((char *)p + i) = 0;
 	}
 
 	return (s);
