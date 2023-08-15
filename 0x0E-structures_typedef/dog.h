ef56687caba7eed8dafd01a5bf50b28b88898242
Precious Aleaji <legenduz15@gmail.com>
2023-08-15 02:40:30 +0000

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - a dog struct
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ *
+ * Description: really
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* _DOG_H_ */
