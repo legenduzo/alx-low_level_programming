b7aa345c2c32bb2b148d3df551b9be611c178bd9
Precious Aleaji <legenduz15@gmail.com>
2023-08-15 09:36:48 +0000

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,17 +12,17 @@ void print_dog(struct dog *d)
 		return;
 
 	if (!d->name)
-		printf("Name: nil\n");
+		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	if (!d->age)
-		printf("Name: 0\n");
+		printf("Age: %f\n", 0);
 	else
 		printf("Age: %f\n", d->age);
 
 	if (!d->owner)
-		printf("Name: nil\n");
+		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
 }
