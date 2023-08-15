5a3cac1bee33c54d37a83edf67c88772e84403d0
Precious Aleaji <legenduz15@gmail.com>
2023-08-15 02:05:26 +0000

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - writes the name of the file and a new line
+ * Description: this describes it
+ *
+ * Return: 0 always success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
