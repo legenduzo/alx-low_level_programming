25b5ce83b56f49d834b15482d6873d33ecc1011a
Precious Aleaji <legenduz15@gmail.com>
2023-08-17 08:07:43 +0000

    3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,14 +17,14 @@ int main(int argc, char *argv[])
 
 	int (*op_func)(int, int);
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
 	if (b == 0 && (strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0))
 	{
