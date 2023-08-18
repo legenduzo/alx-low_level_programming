c70d9029f54fc09603a137650e856f451c94d1c2
Precious Aleaji <legenduz15@gmail.com>
2023-08-18 04:32:56 +0000

    3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,69 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_arg - prints arguments
+ * @format: format
+ * @args: arguments
+ * @sep: separator
+ *
+ * Return: void
+ */
+
+void print_arg(char format, va_list *args, char **sep)
+{
+	char *str;
+
+	switch (format)
+	{
+		case 'c':
+			printf("%s%c", *sep, va_arg(*args, int));
+			*sep = ", ";
+			break;
+		case 'i':
+			printf("%s%d", *sep, va_arg(*args, int));
+			*sep = ", ";
+			break;
+		case 'f':
+			printf("%s%f", *sep, va_arg(*args, double));
+			*sep = ", ";
+			break;
+		case 's':
+			str = va_arg(*args, char *);
+			if (str)
+			{
+				printf("%s%s", *sep, str);
+				*sep = ", ";
+			}
+			else
+			{
+				printf("%s%s", *sep, str);
+				*sep = ", ";
+			}
+			break;
+	}
+}
+/**
+ * print_all - prints diff data types
+ * @format: the format to print
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *sep = "";
+	int i = 0;
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		print_arg(format[i++], &args, &sep);
+	}
+	printf("\n");
+
+	va_end(args);
+}
