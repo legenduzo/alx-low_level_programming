f0460c9bc985c0ef43f3e9a97b5d9386313a725f
Precious Aleaji <legenduz15@gmail.com>
2023-08-18 02:07:24 +0000

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,9 +19,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (!separator)
 		separator = "";
 
-	if (!args)
-		return;
-
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
