108bfbf6497de4b1b727e702387d1798870a8b10
Precious Aleaji <legenduz15@gmail.com>
2023-08-25 02:20:08 +0000

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints a single linked list
+ * @h: pointer to the list to print
+ *
+ * Return: size of list printed
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	if (!h)
+	{
+		printf("[0] (nil)");
+		return (0);
+	}
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		n += 1;
+	}
+	return (n);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is the readme at the root of 0x12-singly_linked_lists
