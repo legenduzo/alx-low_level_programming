2e0b3580ac6e2c9f0e1f0f5049daedfe93a05f09
Precious Aleaji <legenduz15@gmail.com>
2023-08-25 02:36:00 +0000

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,13 @@ size_t print_list(const list_t *h)
 
 	if (!h)
 	{
-		printf("[0] (nil)");
+		printf("[0] (nil)\n");
 		return (0);
 	}
 	while (h != NULL)
 	{
 		if (!h->str)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
