054f59cadef39c521a67ded9fc3f0de76d2430b4
Precious Aleaji <legenduz15@gmail.com>
2023-08-26 02:39:09 +0000

    101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+    hello db 'Hello, Holberton',0
+
+section .text
+    global main
+    extern printf
+
+main:
+    ; Prepare arguments for printf
+    mov rdi, hello ; The format string to print
+    xor rax, rax   ; We need to zero out rax, according to ABI
+    ; Call printf
+    call printf
+
+    ; Prepare to end the program
+    mov eax, 60    ; syscall number for exit
+    xor edi, edi   ; return 0
+    syscall
