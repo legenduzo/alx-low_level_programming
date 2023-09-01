ec8ef22da0caaabd788ae3f6a2ef30fb7e71ee56
Precious Aleaji <legenduz15@gmail.com>
2023-09-01 04:24:45 +0000

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "main.h" 
+
+/**
+ * binary_to_uint - converts bits to decimals
+ * @b: pointer to the string to convert
+ *
+ * Return: 0 on failure, int on success
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	int i;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+
+		n = n * 2 + (b[i] - '0');
+	}
+	return (n);
+}
