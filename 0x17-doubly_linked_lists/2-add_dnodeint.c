b9f9547cd74b8d234d01c71dda56d42e8cffc064
Precious Aleaji <legenduz15@gmail.com>
2023-10-13 01:36:24 +0100

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,8 +21,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
+	if (*head)
+	{
+		new->next = *head;
+		(*head)->prev = new;
+	}
+	else
+		new->next = NULL;
 	*head = new;
 
 	return (new);
