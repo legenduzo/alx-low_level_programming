4d29058dd80335f6703075a217e44d35dfe0a405
Precious Aleaji <legenduz15@gmail.com>
2023-10-17 04:33:30 +0100

    100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,62 @@
+/**
+ * add - adds 2 bumbers and returns the result
+ * @a: first number
+ * @b: second number
+ *
+ * Return: int
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts 2 bumbers and returns the result
+ * @a: first number
+ * @b: second number
+ *
+ * Return: int
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies 2 bumbers and returns the result
+ * @a: first number
+ * @b: second number
+ *
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides 2 bumbers and returns the result
+ * @a: first number
+ * @b: second number
+ *
+ * Return: int
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - mods 2 bumbers and returns the result
+ * @a: first number
+ * @b: second number
+ *
+ * Return: int
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
