a8f379721fc68b74e1533e8acfbcce29f69435c7
Precious Aleaji <legenduz15@gmail.com>
2023-11-04 18:32:22 +0100

    4

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -8,8 +8,17 @@
 int main(void)
 {
     hash_table_t *ht;
+    char *value;
 
     ht = hash_table_create(1024);
-    hash_table_set(ht, "betty", "cool");
+    hash_table_set(ht, "Holberton", "is cool");
+    value = hash_table_get(ht, "Holberton");
+    printf("%s:%s\n", "Holberton", value);
+    hash_table_set(ht, "Holberton", "is soooo cooool");
+    value = hash_table_get(ht, "Holberton");
+    printf("%s:%s\n", "Holberton", value);
+    hash_table_set(ht, "Holberton", "is awesome");
+    value = hash_table_get(ht, "Holberton");
+    printf("%s:%s\n", "Holberton", value);
     return (EXIT_SUCCESS);
 }
---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves the value of key
+ * @ht: hash table
+ * @key: key to use
+ *
+ * Return: the value or null
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	char *value;
+	unsigned long int index;
+
+	if (!ht || !key)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	if (index >= ht->size)
+	       return (NULL);	
+
+	if (!(ht->array[index]))
+		return (NULL);
+
+	value = ht->array[index]->value;
+	return (value);
+}
