b76bf7fdf2c7796032be6a3745815502a52ecea5
Precious Aleaji <legenduz15@gmail.com>
2023-11-04 19:32:42 +0100

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,12 +12,26 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *node;
+	hash_node_t *node, *temp;
 
 	if (!ht || !key || *key == '\0' || !value)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (!(temp->value))
+				return (0);
+			return (1);
+		}
+		temp = temp->next;
+	}
 
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -9,6 +9,8 @@ int main(void)
 {
     hash_table_t *ht;
     char *value;
+    unsigned long int index;
+    int i = 0;
 
     ht = hash_table_create(1024);
     hash_table_set(ht, "Holberton", "is cool");
@@ -20,5 +22,13 @@ int main(void)
     hash_table_set(ht, "Holberton", "is awesome");
     value = hash_table_get(ht, "Holberton");
     printf("%s:%s\n", "Holberton", value);
+
+    index = key_index((unsigned char *)"Holberton", 1024);
+    while (ht->array[index])
+    {
+	    printf("%d: %s\n", i, ht->array[index]->value);
+	    ht->array[index] = ht->array[index]->next;
+	    i++;
+    }
     return (EXIT_SUCCESS);
 }
