e0487788b308696af73621ef599923c80ec4ec5f
Precious Aleaji <legenduz15@gmail.com>
2023-11-04 05:28:15 +0100

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates hash tables
+ * @size: size of the array
+ *
+ * Return: null or hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = NULL;
+	unsigned long int i;
+
+	if (size < 1)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+
+	ht->array = malloc(size *sizeof(hash_node_t *));
+	if (!(ht->array))
+	{
+		free(ht);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	ht->size = size;
+
+	return (ht);
+}
