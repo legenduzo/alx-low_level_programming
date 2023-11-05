d357cbc563688ab9607b46f5dc451780e0f04321
Precious Aleaji <legenduz15@gmail.com>
2023-11-05 09:45:19 +0100

    5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash_table
+ * @ht: the hash table to print
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	char *key, *value;
+	hash_node_t *node, *temp;
+	int flag = 0;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp)
+		{
+			node = temp;
+			temp = temp->next;
+			if (flag)
+				printf(", ");
+
+			key = node->key;
+			value = node->value;
+			if (temp)
+				printf("'%s': '%s', ", key, value);
+			else
+				printf("'%s': '%s'", key, value);
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
